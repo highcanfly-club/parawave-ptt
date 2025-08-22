b25ec6279adb957a77495cd61f6d5d398e101293
Ronan L <aeltorio@universidadatlantico.org>
2025-08-22 12:06:25 +0200

    fix(navbar): update brand name to use site configuration

--- apps/backend-admin/src/components/navbar.tsx
+++ apps/backend-admin/src/components/navbar.tsx
@@ -99,7 +99,7 @@ export const Navbar = () => {
             href="/"
           >
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">{siteConfig().name}</p>
           </Link>
         </NavbarBrand>
         <div className="hidden lg:flex gap-4 justify-start ml-2">
